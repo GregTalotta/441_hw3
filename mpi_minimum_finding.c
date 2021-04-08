5ac62e5093a65dbf0ab66e9377ef195e2db039c6
Gregory Talotta <gjtalotta@alaska.edu>
2021-04-07 21:10:33 -0900

    fixed print statement

--- mpi_minimum_finding.c
+++ mpi_minimum_finding.c
@@ -53,7 +53,7 @@ int main(int argc, char *argv[])
                 min = temp[0];
             }
         }
-        printf("Minimal value is: ", temp);
+        printf("Minimal value is: %d\n", temp);
     }
     free(a);
     free(temp);
