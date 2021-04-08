279b6ca764efb430b9021c027f229cf1698114d5
Gregory Talotta <gjtalotta@alaska.edu>
2021-04-08 14:58:59 -0900

    clean up code

--- again_arr2dsum.cu
+++ again_arr2dsum.cu
@@ -1,9 +1,9 @@
 #include "stdio.h"
 #define DIM 8
 const int THREADS_PER_BLOCK = 8;
-const int NUM_BLOCKS = 8; 
+const int NUM_BLOCKS = 8;
 
-__global__ void add(int* a, int* c)
+__global__ void add(int *a, int *c)
 {
     __shared__ int cache[THREADS_PER_BLOCK];
     int tid = threadIdx.x + (blockIdx.x * blockDim.x);
@@ -22,32 +22,33 @@ __global__ void add(int* a, int* c)
     }
     if (threadIdx.x == 0)         // if at thread 0 in this block
         c[blockIdx.x] = cache[0]; // save the sum in global memory
-
 }
 
 int main()
 {
     int a[DIM][DIM], c[DIM];
-    int* dev_a, * dev_c;
+    int *dev_a, *dev_c;
 
-    cudaMalloc((void**)&dev_a, DIM * DIM * sizeof(int));
-    cudaMalloc((void**)&dev_c, DIM * sizeof(int));
+    cudaMalloc((void **)&dev_a, DIM * DIM * sizeof(int));
+    cudaMalloc((void **)&dev_c, DIM * sizeof(int));
 
-    for (int y = 0; y < DIM; y++)              // Fill Arrays
+    for (int y = 0; y < DIM; y++) // Fill Arrays
         for (int x = 0; x < DIM; x++)
             a[y][x] = 7;
 
-    for(int i = 0; i < DIM; ++i){
-        c[i]=0;
+    for (int i = 0; i < DIM; ++i)
+    {
+        c[i] = 0;
     }
+
     cudaMemcpy(dev_a, a, DIM * DIM * sizeof(int), cudaMemcpyHostToDevice);
 
-    
-    add <<<NUM_BLOCKS, THREADS_PER_BLOCK >>> (dev_a, dev_c);
+    add<<<NUM_BLOCKS, THREADS_PER_BLOCK>>>(dev_a, dev_c);
 
     cudaMemcpy(c, dev_c, DIM * sizeof(int), cudaMemcpyDeviceToHost);
     int total = 0;
-    for(int i = 0; i < DIM; ++i){
+    for (int i = 0; i < DIM; ++i)
+    {
         total += c[i];
         printf("c is: %d\n", c[i]);
     }
