dcfdb971507264c56adc9ccf7302145a0aa578f9
Gregory Talotta <gjtalotta@alaska.edu>
2021-04-08 12:49:53 -0900

    un com all

--- cuda_minimum_finding.cu
+++ cuda_minimum_finding.cu
@@ -6,21 +6,21 @@
 
 __global__ void find_min(int *a, int *c)
 {
-    // int rank = threadIdx.x;
-    // int p = sizeof(c)/sizeof(int);
-    // int numToSort = (8 * 1000000) / p;
-    // int low = rank * numToSort;
-    // int high = low + numToSort - 1;
-    // int min = a[low];
-    // for (int i = low; i < high; ++i)
-    // {
-    //     if (min > a[i])
-    //     {
-    //         min = a[i];
-    //     }
-    // }
-    // c[rank] = min;
-    // printf("crash here 2.5 \n");
+    int rank = threadIdx.x;
+    int p = sizeof(c)/sizeof(int);
+    int numToSort = (8 * 1000000) / p;
+    int low = rank * numToSort;
+    int high = low + numToSort - 1;
+    int min = a[low];
+    for (int i = low; i < high; ++i)
+    {
+        if (min > a[i])
+        {
+            min = a[i];
+        }
+    }
+    c[rank] = min;
+    printf("crash here 2.5 \n");
 }
 
 int main()
@@ -33,32 +33,32 @@ int main()
     int *dev_c;
 
     printf("crash here 0 \n");
-    // cudaMalloc((void**)&dev_a, N * sizeof(int));
-    // cudaMalloc((void**)&dev_c, T * sizeof(int));
-    //fill the array
-    // for (int i = 0; i < N; ++i)
-    // {
-    //     a[i] = rand() % 1000000000;
-    // }
+    cudaMalloc((void**)&dev_a, N * sizeof(int));
+    cudaMalloc((void**)&dev_c, T * sizeof(int));
+    fill the array
+    for (int i = 0; i < N; ++i)
+    {
+        a[i] = rand() % 1000000000;
+    }
 
     printf("crash here 1 \n");
-    // cudaMemcpy(dev_a, a, N * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_a, a, N * sizeof(int), cudaMemcpyHostToDevice);
     printf("crash here 2 \n");
-    // dim3 grid(1);
-    // dim3 threads(T);
-    // find_min <<<grid, threads >>> (dev_a, dev_c);
+    dim3 grid(1);
+    dim3 threads(T);
+    find_min <<<grid, threads >>> (dev_a, dev_c);
 
-    // cudaMemcpy(c, dev_c, N * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(c, dev_c, N * sizeof(int), cudaMemcpyDeviceToHost);
 
     printf("crash here 3 \n");
-    // cudaDeviceSynchronize();  // Waits for threads to finish
-    // int min = c[0];
-    // for(int i = 0; i < T; ++i){
-    //     if(min > c[i]){
-    //         min = c[i];
-    //         printf("Minimal value parallel with cuda is: %d\n", min);
-    //     }
-    // }
+    cudaDeviceSynchronize();  // Waits for threads to finish
+    int min = c[0];
+    for(int i = 0; i < T; ++i){
+        if(min > c[i]){
+            min = c[i];
+            printf("Minimal value parallel with cuda is: %d\n", min);
+        }
+    }
     return 0;
 }
 
