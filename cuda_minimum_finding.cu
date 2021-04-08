6da3c9e5f6e6b898618d8003b1e44de7955e777e
Gregory Talotta <gjtalotta@alaska.edu>
2021-04-08 12:27:06 -0900

    added print statements

--- cuda_minimum_finding.cu
+++ cuda_minimum_finding.cu
@@ -23,6 +23,7 @@ __global__ void min(int *a, int *c)
     int low = rank * numToSort;
     int high = low + numToSort - 1;
     c[rank] = findMinimum(a, low, high);
+    printf("crash here 2.5 \n");
 }
 
 int main()
@@ -38,12 +39,15 @@ int main()
     {
         a[i] = rand() % 1000000000;
     }
+    printf("crash here 1 \n");
     cudaMemcpy(dev_a, a, N * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(dev_c, c, num_threads * sizeof(int), cudaMemcpyHostToDevice);
+    printf("crash here 2 \n");
     dim3 grid(1);
     dim3 threads(num_threads);
     min <<<grid, threads >>> (dev_a, dev_c);
     cudaMemcpy(c, dev_c, N * sizeof(int), cudaMemcpyDeviceToHost);
+    printf("crash here 3 \n");
     cudaDeviceSynchronize();  // Waits for threads to finish
     int min = c[0];
     for(int i = 0; i < num_threads; ++i){
